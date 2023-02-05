7d9c7a41aec27777d776314d531789ccbba5b89a
Gilgameshgb-1 <farisbazdar1@gmail.com>
2023-02-05 19:03:54 +0100

    minor changes

--- script.py
+++ script.py
@@ -1,42 +1,46 @@
-# bot.py
 import os
-
 import random
 import discord
+
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
+@bot.command(name='test', help = 'Respons with a random line from the Backstreets boy "I want it that way."')
+async def nine_nine(ctx):
+    test = ['aint nothing but a heartache',
+                'aint nothing but a mistake',
+                'i dont ever wanna hear you say',
+                'i want it that way']
 
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my Discord server!'
-    )
+    response = random.choice(test)
+    await ctx.send(response)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    tmp = [
-        'https://www.klix.ba/biznis/finansije/hrvatska-namjerava-prodavati-narodne-obveznice-o-cemu-je-rijec-i-da-li-bih-treba-uraditi-isto/230205030'
+@bot.command(name='rollDice', help='Simulates rolling dice.')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
     ]
+    await ctx.send('The dice rolls are: ')
+    await ctx.send(', '.join(dice))
+    await ctx.send('Ako citas ovo, volim te. <3')
 
-    if message.content == 'test!':
-        response = random.choice(tmp)
-        await message.channel.send(response)
+@bot.command(name='createChannel')
+@commands.has_role('admin')
+async def create_channel(ctx, channel_name):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
 
-client.run(TOKEN)
+bot.run(TOKEN)
